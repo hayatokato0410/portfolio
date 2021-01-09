02f8b8633070dfb482672424bf149c870336548d
hayatokato0410 <hayatokato0410@gmail.com>
2021-01-10 01:50:49 +0900

    change

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,19 +31,8 @@ export default {
   modules: [
   ],
 
-  // // Build Configuration (https://go.nuxtjs.dev/config-build)
-  // build: {
-  // }
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-    extend ( config, { isDev, isClient, isServer } ) {
-      if ( isServer ) {
-        config.externals = {
-          '@firebase/app': 'commonjs @firebase/app',
-          '@firebase/firestore': 'commonjs @firebase/firestore',
-          //etc...
-        }
-      }
-    }
-}
+  }
 }
 
