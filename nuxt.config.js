570b3a1490861e0132c5a875033b7f75bf2cf126
hayatokato0410 <hayatokato0410@gmail.com>
2021-02-11 18:27:44 +0900

    up

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,9 +14,9 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
-    { src: '~assets/css/style.css' }
-    ],
-
+    '~/assets/css/style.css',
+    '~/assets/css/reset.css',
+  ],
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
   ],
